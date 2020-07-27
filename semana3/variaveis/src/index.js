b12a05abbc306340e234f9f68dca65f397de400f
Juan Albuquerque <juankleyton@gmail.com>
2020-07-27 15:43:46 -0300

    exercicio variaveis

---
+++ semana3/variaveis/src/index.js
@@ -0,0 +1,95 @@
+//interpretação
+
+//1- será impresso b = 10 na primeira linha, na segunda linha a= 10 e b=5
+//2-será impresso a= 20 b= undefined c= 10
+
+// escrita de código
+
+// a) Declare uma variável para armazenar um nome, sem atribuir um valor
+
+    //let nome
+    
+    
+
+   // b) Declare uma variável para armazenar uma idade, sem atribuir um valor.
+    //let idade
+   
+   
+
+   // c) Imprima na tela o tipo dessas variáveis que acabou de criar, usando o comando typeof
+   
+   //console.log(typeof nome, typeof idade)
+
+   //d) Reflita: por que esse tipo foi impresso? Escreva a resposta em um comentário de código.
+
+        // O tipo tá representando que a variável não possui nenhum valor
+
+    //d) Pergunte ao usuário seu nome e sua idade, atribuindo esses dois valores às variáveis que acabou de criar.
+            /*
+            const nome = prompt("Qual o seu nome?")
+            const idade = prompt ("Qual a sua idade?")
+            */
+
+      // "e) Novamente, imprima na tela o tipo dessas variáveis. O que você notou? Escreva em um comentário de código."       
+
+            //console.log(typeof nome, typeof idade)
+
+            // Agora as duas variaveis possuem valores, seus tipos mudaram para strings
+
+     //f) Para finalizar, imprima na tela a mensagem: "Olá nome,  você tem idade anos". Onde nome e idade são os valores que o usuário inseriu
+
+         //console.log("Olá", nome ,'você tem' ,idade, 'anos')
+
+
+
+      // 2- Faça um programa que faça 5 perguntas para o usuário (pode ser criativo aqui). Imprima-as com as respostas no console da seguinte forma:
+
+         /*   const nome = prompt( "Qual o seu nome")
+            const idade = prompt("Qual a sua idade")
+            const preferido1= prompt("Nescal ou Toddy?")
+            const preferido2 = prompt("Playstation ou Xbox?")
+            const preferido3 = prompt("Digimou ou Pokemon?")
+            console.log("Qual o seu nome?","Resposta:",nome)
+            console.log("Qual a sua idade?","Resposta:",idade)
+            console.log("Nescal ou Toddy?","Resposta:",preferido1)
+            console.log("Playstation ou Xbox?","Resposta:",preferido2)
+            console.log("Digimou ou Pokemon?","Resposta:",preferido3)
+            */
+
+        //    3-Faça um programa que contenha um array com 5 das suas comidas preferidas, armazenado em uma variável. Em seguida, siga os passos:
+
+               /* let array = ["Sushi", "Pizza" , "Chocolate" , "Lazanha", "Sorvete"]
+
+                //a)
+                    console.log(array)
+
+                //b)
+                
+                    console.log("Essas são minhas comidas preferidas:")
+                    console.log(array[0])
+                    console.log(array[1])
+                    console.log(array[2])
+                    console.log(array[3])
+                    console.log(array[4])
+                    
+
+               //c)
+                    let newarray = prompt ("Qual sua comida preferida?")
+                    array[1] = newarray
+                    console.log (array)
+                    
+                    */
+
+                  //4-  Faça um programa que contenha um array com 3 perguntas de Sim ou Não, armazenado em uma variável. Por exemplo: "Você está usando uma roupa azul hoje?". Depois, siga os passos:    
+                        /*
+                        let arraypergu = ["Você gosta de Sushi?", "Você gosta de Sorvete?", "Você gosta de Jambo?"]
+                        let arrayresp = [true, true, false]
+
+                        console.log(arraypergu[0], arrayresp[0])
+                        console.log(arraypergu[1], arrayresp[1])
+                        console.log(arraypergu[2], arrayresp[2])
+                        */
+
+
+                        
+
