95e00046239e17d764856eab0c1eeaa9d649f400
Juan Albuquerque <juankleyton@gmail.com>
2020-07-28 16:18:41 -0300

    exercicio operadores

---
+++ semana3/operadores/src/index.js
@@ -0,0 +1,161 @@
+//Exercícios de interpretação de código
+
+/*1- let resultado = bool1 && bool2
+    console.log("a. ", resultado)
+    a. resultado = 
+     bool1 = true
+     bool2 = false
+       
+    
+    b)
+
+    resultado = bool1 && bool2 && bool3
+    console.log("b. ", resultado)
+
+    "b. ", resultado =
+    bool1 = true
+    bool2 = false
+    bool3 = true  // o "!" negou o false e transformou em true
+
+    c)
+
+    resultado = !resultado && (bool1 || bool1)
+    console.log("c. ", resultado)
+
+    "c. ", resultado = false e true 
+    
+    // boo1 || boo1 ambos true, resultado = !resultado (true), logo, a negação
+    do resultado de boo1 || boo1 = false
+
+    console.log("e. ", typeof resultado)
+    
+    typeof resultado = boolean
+       */
+
+       /* 2-Indique todas as mensagens impressas no console, SEM EXECUTAR o programa.
+
+            a)let array
+            console.log('a. ', array)
+            'a. ', array = undefined
+
+            b) array = null
+            console.log('b. ', array)
+                'b.' , array = null
+
+            c) array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+                console.log('c. ', array.length)
+                'c. ', array.length= 11
+
+            d)let i = 0
+                console.log('d. ', array[i])    
+                'd. ', array[i] = 0
+
+            e) array[i+1] = 19
+              console.log('e. ', array)
+                e. ', array = 19
+                
+            f) const valor = array[i+6]
+               console.log('f. ', valor)   
+               'f. ', valor = [i+6] 
+
+
+
+       */
+
+      //Exercícios de escrita de código
+
+        //a. Pergunte a idade do usuário
+           /* let idade = prompt("Qual a sua idade?")
+                Number = (idade)
+                console.log(idade)
+
+        // b. Pergunte a idade do seu melhor amigo ou da sua melhor amiga
+        
+        let mamiga = prompt("Qual a sua idade melhor amiga?")
+            Number = (mamiga)
+            console.log (mamiga)
+            */
+
+          // c. Imprima na tela a seguinte mensagem: "Sua idade é maior do que a do seu melhor amigo?", seguido pela resposta (true ou false)
+            /*
+                MinhaIdade = 28
+                IdadeAmigo = 23
+                Maior = MinhaIdade > IdadeAmigo
+                console.log("Sua idade é maior que a do seu melhor amigo?", Maior)
+
+        //d. Imprima na tela a diferença de idade (não tem problema se sair um número negativo)
+
+            diferecaIdade = 28 - 23
+            console.log("A diferença de idade é : ", diferecaIdade )
+
+            */
+
+           /*2.Faça um programa que:
+           //a) Peça ao usuário que insira um número par.
+             let par = prompt("Digite um numero par")
+                Number=(par)
+
+            //b)Imprima na tela o resto da divisão desse número por 2.
+                resto = par % 2
+                console.log("O resto da divisão é : ",resto)
+
+            //c. Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de códig
+                // todo numero dividido por 2, tem resto 0.
+
+            //d) O que acontece se o usuário inserir um número ímpar? Escreva em um comentário de código
+
+                // Ao colocar um numero impar, o resto fica 1
+
+            */
+            /*
+            //3.Faça um programa, seguindo os passos:
+
+            //a. Crie um array vazio e guarde-o em uma variável, chamada listaDeTarefas
+             
+            let listaDeTarefas = []
+        
+
+             //b. Pergunte ao usuário 3 tarefas que ele precise realizar no dia e armazene-as, uma por uma, no array
+            const tarefa1 = prompt("Qual a sua primeira tarefa")
+            listaDeTarefas.push(tarefa1)
+            const tarefa2 = prompt("Qual a sua segunda tarefa")
+            listaDeTarefas.push(tarefa2)
+            const tarefa3 = prompt("Qual a sua terceira tarefa")
+            listaDeTarefas.push(tarefa3)
+            //c. imprima o array na tela:
+            console.log(listaDeTarefas)
+    
+            //d. Peça ao usuário que digite o índice de uma tarefa que ele já realizou: 0, 1 ou 2
+                 const indiceTarefa = Number(prompt("Digite o indice de uma de suas tarefas realizadas"))
+                 
+            //e) Remova da lista o item de índice que o usuário escolheu.
+                 listaDeTarefas.splice(indiceTarefa, 1)
+                 console.log(listaDeTarefas)
+
+            */
+
+            
+            
+            /*
+           //4-Faça um programa que pergunte ao usuário seu nome e seu e-mail. Em seguida, imprima na tela a seguinte mensagem:variável, chamada listaDeTarefas
+                //O e-mail emailDoUsuario foi cadastrado com sucesso. Seja bem-vinda(o), nomeDoUsuario!
+                let nomeDoUsuario = prompt("Digite seu nome")
+                let emailDoUsuario = prompt("Digite seu email")
+                console.log ("O e-mail", emailDoUsuario, "foi cadastrado com sucesso. Seja bem-vindo(a),", nomeDoUsuario)
+
+               */ 
+                
+
+
+
+            
+
+
+
+
+
+
+            
+
+      
+
