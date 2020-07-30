f9bb2dff2165b252e5a4a6e91d88cf106d9561de
Juan Albuquerque <juankleyton@gmail.com>
2020-07-30 16:36:58 -0300

    exercicio loops

---
+++ semana3/loops/src/index.js
@@ -0,0 +1,17 @@
+//Exercícios de interpretação de código
+
+/*01 - 
+ R- Enquanto valor for menor que 5, imprima na tela seus antecessores.
+ R- 1,2,3,4
+02 - 
+ a) [19, 21, 23, 25, 27, 30]
+ b)sim, só colocar na condição if (numero <= 30), todos os numeros do array serão impressos.
+ */
+
+ //desafio interpretação de código
+    // serão impressos 4 linhas de zeros, 
+    // a primeira contendo 0, 
+    // a segunda contendo 00, 
+    // a terceira 000
+    // e a quarta 0000
+        
