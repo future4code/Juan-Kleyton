0fba5be51513e4c7f90767b28613ce8490c3e7d3
Juan Albuquerque <juankleyton@gmail.com>
2020-07-30 19:41:07 -0300

    exercicio loops completo

--- semana3/loops/src/index.js
+++ semana3/loops/src/index.js
@@ -14,4 +14,63 @@
     // a segunda contendo 00, 
     // a terceira 000
     // e a quarta 0000
-        
+
+     // Exercícios de escrita de código
+    
+    // 03 - 
+
+    // a. Escreva um programa que:
+
+    // - **Imprima** cada um dos valores do array original.
+    
+    // b. Escreva um programa que:
+    
+    // - **Imprima** cada um dos valores do array original divididos por 10
+    
+    // c. Escreva um programa que:
+    
+    // - **Crie** um novo array contendo, somente, os números pares do array original.
+    // - **Imprima** esse novo array
+    
+    // d. Escreva um programa que:
+    
+    // - **Crie** um novo array contendo strings, da seguinte forma: "O elemento do índex `i` é: `numero`"
+    // - **Imprima** este novo array
+    
+    // e. Escreva um programa que imprima no console o maior e o menor números contidos no array original
+
+    //a)
+   //  const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+   //      console.log(array)
+
+     //b) 
+   //   for(let i = 0; i < array.length; i++){
+   //    console.log(array[i]/10)  
+   //   }
+
+    //c)
+
+   //   let ArrayPar = [80,30,130,40,60,120,90,110]
+   //   for (let i = 0; i < ArrayPar.length; i++) {
+   //   console.log(ArrayPar  [i])
+   //   }
+
+    // d)
+
+   //  let arrayString = ["80", "30", "130", "40", "60", "21", "70", "120", "90", "103", "110", "55"]
+   //  for(let i = 0; i < arrayString.length; i++)
+   //  console.log("o elemento do index ", i ," é " , arrayString[i])
+
+   //e)
+
+   // let array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+   // let valorMax = 0
+   // let valorMin = 131
+   // for(let i = 0; i < array.length; i++){
+   //   if(array[i] > valorMax){
+   //     valorMax = array[i]
+   //  }else if(array[i] < valorMin){
+   //      valorMin = array[i]
+   //  }
+   // }
+   // console.log("O maior elemento é", valorMax, "e o menor é", valorMin)
