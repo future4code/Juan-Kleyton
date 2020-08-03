062355a200b4ae086d42a5b65e91ef9457202a50
Juan Albuquerque <juankleyton@gmail.com>
2020-08-03 16:27:57 -0300

    Exercicio Funcoes

---
+++ semana4/funcoes/src/index.js
@@ -0,0 +1,54 @@
+//Exercícios de interpretação de código
+
+// 1- 
+//a)Imprimirá variavel 2, 5 vezes = 10
+//  e a variavel 10, 5 vezes = 50
+// b) não a acontecia nada, pois não foi impresso no console
+
+//2- 
+// a)imprime "Darvas" e "Goli" pois seus indices são menores que 2
+// b) Imprimirei ambos os nomes, pois seuis indices são menores que 2
+
+//3- pega o resto da divisao que é zero, e multiplica por ele mesmo, resultando assim em um
+//array vazio
+
+//Exercícios de escrita de código
+
+//4-  
+        // function sobre (Nome,Idade,Cidade,estudante){
+        //     if(estudante === true){
+        //         console.log (`Eu sou${Nome}, tenho ${Idade}anos, moro em ${Cidade} e sou Estudante`)
+        //     }else if (estudante === false){
+        //         console.log (`Eu sou ${Nome}, tenho ${Idade}  anos, moro em ${Cidade} e não sou Estudante`)
+        //     }
+        // }
+
+        //     const imprimir = sobre (" Juan ", 28 , "Natal" , true)
+
+//5- 
+    //a)
+        // function somarDoisNumeros(a,b) {
+        //      const soma = a+b
+        //      console.log(soma)
+
+        // }
+
+
+        //     somarDoisNumeros(1, 2)
+
+    //b)
+//     function somarDoisNumeros(a,b) {
+//         if(a > b){
+//             console.log(`O numero ${a} é maior que ${b}`)
+//         } else {
+//             console.log(`O numero ${a} é menor que ${b}`)
+//         }
+
+//    }
+
+
+//        const imprimir = somarDoisNumeros ( 3,2)
+
+//c)
+
+
