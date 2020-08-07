f7e9164824cc7ce1c54468c6df8d828dcc781576
Juan Albuquerque <juankleyton@gmail.com>
2020-08-07 14:58:52 -0300

    Exercicio Interpretação de Codigo

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,82 @@
+// Interpretação de Código
+
+/*
+01- Cria uma função chamada "conversorDeMoeda" para parametro "Valor em dolar", após
+um variável chamada "Cotação", onde pede ao usuário que digite a cotação do dolar, retorna
+o valorEmdolar * cotação
+após cria um variavel "Meu Dinheiro" e atribui um valor 100 ao "conversorDeMoeda"
+imprime o valor de 100 dolares * a cotação informada pelo usuário em reais
+
+*/
+
+
+/*
+02- Cria um função InvesteDinheiro que recebe 2 parametros "tipoDeInvestimento" e "Valor"
+    depois cria uma variavel chamada "valorAposInvestimento"
+    Cria um switch com os seguintes casos: 
+    "Poupança" que recebe a váriável ValorAposInvestimento = valor * 1.03
+    "RendaFixa" que recebe a váriável ValorAposInvestimento = valor * 1.05
+    "CDB" que recebe a váriável ValorAposInvestimento = valor * 1.06
+    "Ações" que recebe a váriável ValorAposInvestimento = valor * 1.1
+    caso não for escolhido esses caso, retorna um alerta "Tipo de investimento informado incorreto"
+    retorna valorAposInvestimento
+    Cria duas váriaveis novoMontante que recebe açoes com valor 150 
+    e SegundoMontante que recebe "Tesouro Direito" com valor 200
+
+    Imprime novoMontate que será igual a 150 * 1.1
+    Imprime segundoMontate que receberá o Alert "Tipo de investimento informado incorreto"
+
+*/
+
+/*
+    03- Cria um constante "numero" que recebe um array de numeros
+        cria uma contate "array1" que recebe um array vazio
+        cria uma costante "array2" que recebe um array vazio 
+
+        se a divisao dos indices por 2 resultar em resto 0
+        Coloca o numero no array1
+        se não
+        coloca o numero no array2
+
+        imprime a quantidade de indices no array numeros
+        imprime a quantidade de indices no array1
+        imprime a quantidade de indices no array2
+*/
+
+
+/*
+    4- cria uma constante "Numero" com um array com 26 indices
+        uma variavel numero1 que recebe infinity
+        uma variavel numero2 que recebe 0
+
+        se o indice numero for menor que infinito, variavel numero1 = ao indice "numero
+        se o indice numero for maior que 0, variavel numero2 = ao indice "numero"
+
+        imprime todo o array pois todos os indices são menos que infinito
+        imprime todo o array pois todos os indices são maiores que 0
+*/
+
+/*
+
+*/
+/*
+
+*/
+/*
+
+*/
+/*
+
+*/
+/*
+
+*/
+/*
+
+*/
+/*
+
+*/
+/*
+
+*/
