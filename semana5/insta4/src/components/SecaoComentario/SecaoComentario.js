df46fdd8c2c01062b70cbd25f6c75bf810c90b3e
Juan Albuquerque <juankleyton@gmail.com>
2020-08-12 15:48:22 -0300

    Insta4-React

---
+++ semana5/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import './SecaoComentario.css'
+
+export class SecaoComentario extends Component {
+	state = {
+
+	}
+
+	onChangeComentario =(event) => {
+		this.setState ({comentario: event.target.value})
+
+	}
+
+	render() {
+		console.log(this.state.comentario)
+		return( 
+
+		<div className={'comment-container'}>
+
+			<input
+				className={'input-comentario'}
+				placeholder={'Comentário'}
+				value={this.state.comentario}
+				onChange={this.onChangeComentario}
+			/>
+			<button onClick={this.props.aoEnviar}>Enviar</button>
+		</div>
+		)
+	}
+}
