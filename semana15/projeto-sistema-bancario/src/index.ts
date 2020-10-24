e6f551c28b4e3741ea150c6ccc9e47b28debdf24
Juan Albuquerque <juankleyton@gmail.com>
2020-10-24 14:23:04 -0300

    Criando primeiro endpoint

--- semana15/projeto-sistema-bancario/src/index.ts
+++ semana15/projeto-sistema-bancario/src/index.ts
@@ -1,10 +1,24 @@
 import express, {Express, Request, Response} from "express"
 import cors from "cors"
+import {accounts} from "./accounts"
 
 const app:Express = express()
 
 app.use(express.json)
    app.use(cors())
+
+   app.post("/users/create", (Request,Response) =>{
+
+    try{
+        //validar as entradas de requisição
+        // consultar ou alterar a base dados
+        // validar os resultados da consulta
+        // enviar as resposta
+    } catch(error){
+
+    }
+
+   })
 
    app.listen(3003,()=>{
     console.log("Server is running in port 3003")
