80e5a131948de6c17c38ada4cf12a3072a4b4780
juan <juan-dev@pop-os.localdomain>
2020-11-03 12:56:43 -0300

    exercicio-aula48

--- semana15/projeto-sistema-bancario/src/index.ts
+++ semana15/projeto-sistema-bancario/src/index.ts
@@ -7,15 +7,31 @@ const app:Express = express()
 app.use(express.json)
    app.use(cors())
 
-   app.post("/users/create", (Request,Response) =>{
+   app.post("/users/create", (req:Request,res:Response) =>{
 
     try{
         //validar as entradas de requisição
         // consultar ou alterar a base dados
+         const {name,CPF,dateOfBirthAsString} = req.body
+
+         const [day,month, year] = dateOfBirthAsString.split("/")
+
+         const dateOfBirth: Date = new Date(`${year}-${month}-${day}`)
+
+         accounts.push({
+             name,
+             CPF,
+             dateOfBirth,
+             balance: 0,
+             statement:[]
+         })
         // validar os resultados da consulta
         // enviar as resposta
-    } catch(error){
 
+        res.status(201).send("Account Send of Sucess")
+    } catch(error){
+        res.status(400).send(error.message)
+        console.log(error)
     }
 
    })
